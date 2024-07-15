2a4dee9e846b26280f450b0dfbdfe4161439a479
Jay Telgote <120390468+ijaytelgote@users.noreply.github.com>
2024-07-15 10:02:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def recommendation():
     return jsonify(recommend(resume,jds,threshold))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
